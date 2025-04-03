d8bf1bd6fe62a24f2b1a83ce99e5984fa886bc3c
mohammadaminaliyari <mohammadaminaliyari@gmail.com>
2025-04-03 15:16:20 +0330

    Update main.py

--- main.py
+++ main.py
@@ -21,7 +21,7 @@ else:
 
 # لاگین به اینستاگرام با session
 loader = instaloader.Instaloader()
-loader.load_session_from_file(INSTAGRAM_SESSION_USER)
+loader.load_session_from_file("moshaveranpooya")
 
 for username in INSTAGRAM_USERS:
     try:
